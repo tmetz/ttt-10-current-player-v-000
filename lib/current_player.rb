c5f2c3c13e0119aec0baf22765b53255fa88845e
Tammy Metz <tammy@tammymetz.com>
2018-06-29 19:24:05 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  count = 0
+  board.each do |this_space|
+    if this_space == "X" || this_space == "O"
+      count += 1
+    end
+  return count
+end
+
+def current_player(board)
+  how_many_turns = turn_count(board)
+  if how_many_turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
