0246535395fb3dd3454405815bfc92feeb9979dd
Tammy Metz <tammy@tammymetz.com>
2018-06-29 19:30:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  count = 0
+  board.each do |this_space|
+    if this_space == "X" || this_space == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  how_many_turns = turn_count(board)
+  how_many_turns % 2 == 0 ? "X" : "O" # ternary operator
+#  if how_many_turns % 2 == 0
+#    return "X"
+#  else
+#    return "O"
+#  end
+end
